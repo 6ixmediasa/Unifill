09ab6cc0cbde5f1c83c217edd0a502dc08077021
Farai <91912065+6ixmediasa@users.noreply.github.com>
2026-08-26 01:08:47 +0800

    Fix logo image style typing

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,3 +1,3 @@
 import React from 'react';
-import {Image, ViewStyle} from 'react-native';
-export function Logo({height=42,style}:{height?:number;style?:ViewStyle}){return <Image source={require('@/assets/logo-horizontal.png')} resizeMode="contain" style={[{height,width:height*2.3},style]}/>}
+import {Image, ImageStyle, StyleProp} from 'react-native';
+export function Logo({height=42,style}:{height?:number;style?:StyleProp<ImageStyle>}){return <Image source={require('@/assets/logo-horizontal.png')} resizeMode="contain" style={[{height,width:height*2.3},style]}/>}
